343a7574d81abfe3131d5a8e6502103edba9e5b4
Yu-HuanWu <yuhuanhwu@gmail.com>
2023-12-01 22:53:41 -0800

    finished day2

--- Day02/day2.js
+++ Day02/day2.js
@@ -9,7 +9,6 @@ fs.readFile('input', 'utf8', (err, data) => {
     // part 1
     let numbers = []
     data.split("\n").forEach((datum) => {
-        console.log(datum)
         let id = datum.split(':')[0].split(' ')[1];
         let possible = true
 
@@ -31,43 +30,29 @@ fs.readFile('input', 'utf8', (err, data) => {
     console.log(numbers, numbers.reduce((partialSum, a) => partialSum + a, 0))
 
     // part 2
-    // let newData = []
-    // data.split("\n").forEach((datum) => {
-    //     let newDatum = datum
-    //     let newNumber = []
-    //     for (let i = 0; i < newDatum.length; i++) {
-    //         if (oneToNine.includes(newDatum[i])) {
-    //             newNumber.push(newDatum[i])
-    //         } else {
-    //             let currSub = newDatum.substring(i, i + 5)
-    //             alphaOneToNine.forEach(num => {
-    //                 if (currSub[0] === num[0] && currSub.includes(num)) {
-    //                     // newDatum = newDatum.replace(num, (alphaOneToNine.indexOf(num)+1))
-    //                     newNumber.push((alphaOneToNine.indexOf(num) + 1).toString())
-    //                 }
-    //             })
-    //         }
-    //     }
-    //     newData.push(newNumber)
-    // })
+    let sum = 0
+    data.split("\n").forEach((datum) => {
 
-    // const numbers2 = []
-    // newData.forEach((datum) => {
-    //     const currNum = []
-    //     datum.forEach(char => {
-    //         if (oneToNine.includes(char)) {
-    //             if (char === '0') { console.log(datum) }
-    //             currNum.push(char)
-    //         }
-    //     })
-    //     if (currNum.length === 1) {
-    //         const temp = [currNum[0], currNum[0]]
-    //         numbers2.push(parseInt(temp.join('')))
-    //     } else {
-    //         const temp = [currNum[0], currNum[currNum.length - 1]]
-    //         numbers2.push(parseInt(temp.join('')))
-    //     }
-    // });
+        let minRed = 0
+        let minBlue = 0
+        let minGreen = 0
+        datum.split(': ')[1].split('; ').forEach(set => {
+            set.split(', ').forEach(dice => {
+                let amount = parseInt(dice.split(' ')[0]);
+                let color = dice.split(' ')[1];
+                if (color === 'red' && amount > minRed) {
+                    minRed = amount;
+                } else if (color === 'blue' && amount > minBlue) {
+                    minBlue = amount;
+                } else if (color === 'green' && amount > minGreen) {
+                    minGreen = amount
+                }
+            })
+        })
+        let power = minRed * minBlue * minGreen
+        sum = sum + power
+    });
 
+    console.log('sum = ', sum)
     // console.log(numbers2.reduce((partialSum, a) => partialSum + a, 0))
 });
