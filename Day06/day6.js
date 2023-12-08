c78f82c9d9a1386e0da7e7228df66a67b57fbcf4
Yu-HuanWu <yuhuanhwu@gmail.com>
2023-12-08 11:42:29 -0800

    prepping day 6

---
+++ Day06/day6.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.readFile('input', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    // part 1
+
+    data.split("\n\n").forEach((datum, idx) => {
+
+    });
+
+});
